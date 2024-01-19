7c346583658fef53a50b57fdb0ba4d5cf66a9ba0
Mansi Gusain <112713398+GusainMansi@users.noreply.github.com>
2024-01-19 12:52:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 //your JS code here. If required.
-const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
-list = document.getElementById("band")
+// const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+// list = document.getElementById("band")
 
-const sortedList =()=>{
-	sortedBand = bands.sort((a, b)=>{
-		if (a.replace("The ", "").replace("An ", "").replace("A ", "") > b.replace("The ", "").replace("An ", "").replace("A ", ""))
-			return 1;
-		else return -1
-	})
-	sortedBand.forEach((item) => {
-		  list.appendChild( document.createElement("li").appendChild(document.createTextNode(item)));
-	})
-}
+// const sortedList =()=>{
+// 	sortedBand = bands.sort((a, b)=>{
+// 		if (a.replace("The ", "").replace("An ", "").replace("A ", "") > b.replace("The ", "").replace("An ", "").replace("A ", ""))
+// 			return 1;
+// 		else return -1
+// 	})
+// 	sortedBand.forEach((item) => {
+// 		  list.appendChild( document.createElement("li").appendChild(document.createTextNode(item)));
+// 	})
+// }
 
-sortedList()
+// sortedList()
